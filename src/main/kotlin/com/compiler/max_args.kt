671fa43a21b51731d3b5f028d9479424091af88c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-03-04 13:33:12 +0000

    Return number of arguments to print in compound statements

--- src/main/kotlin/com/compiler/max_args.kt
+++ src/main/kotlin/com/compiler/max_args.kt
@@ -1,8 +1,17 @@
 package com.compiler
 
-fun maxArgs(statement: Statement): Int =
-    when (statement) {
+fun maxArgs(statement: Statement): Int {
+    return when (statement) {
         is Print -> statement.expressions.size
         is Assignment -> 0
-        else -> TODO()
+        is CompoundStatement ->
+            statement
+                .findPrint()?.let { print -> maxArgs(print) }
+                ?: run { TODO() }
     }
+}
+
+fun CompoundStatement.findPrint(): Print? =
+    if (statement1 is Print) statement1
+    else if (statement2 is Print) statement2
+    else null
--- src/test/kotlin/com/compiler/MaxArgsTest.kt
+++ src/test/kotlin/com/compiler/MaxArgsTest.kt
@@ -3,7 +3,8 @@ package com.compiler
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
-
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class MaxArgsTest {
     @Test fun `it returns the number of arguments in a print statement`() {
@@ -18,10 +19,32 @@ class MaxArgsTest {
         assertThat(maxArgs(statement), equalTo(2))
     }
 
+    @ParameterizedTest
+    @MethodSource("compoundStatementsContainingPrintAnIdentifierValueAndAssignment")
+    fun `it returns the number of arguments for a print as part of a compound statement`
+                (compoundStatement: CompoundStatement) {
+        assertThat(maxArgs(compoundStatement), equalTo(1))
+    }
+
     @Test fun `it returns 0 for an assignment`() {
         assertThat(
             maxArgs(Assignment("a", Number(1))),
             equalTo(0)
         )
+    }
+
+    companion object {
+        @JvmStatic
+        fun compoundStatementsContainingPrintAnIdentifierValueAndAssignment() =
+            listOf(
+                CompoundStatement(
+                    Assignment("a", Number(1)),
+                    Print(TerminalExpression(Identifier("a")))
+                ),
+                CompoundStatement(
+                    Print(TerminalExpression(Number(7))),
+                    Assignment("a", Number(1)),
+                ),
+            )
     }
 }
