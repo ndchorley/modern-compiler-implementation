328081c0bb9b206b9c454fd4246377c899ec26f9
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-05-14 17:43:22 +0100

    Remove id type alias

--- ocaml/compiler/lib/grammar.ml
+++ ocaml/compiler/lib/grammar.ml
@@ -1,14 +1,12 @@
-type id = string
-
 type binary_operator = Plus | Minus | Times | Divide
 
 type statement =
   | CompoundStatement of statement * statement
-  | Assignment of id * expression
+  | Assignment of string * expression
   | Print of expression list
 and
 expression =
-  | Identifier of id
+  | Identifier of string
   | Number of int
   | BinaryExpression of expression * binary_operator * expression
   | StatementThenExpression of statement * expression
