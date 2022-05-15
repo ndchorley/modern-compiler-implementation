bc7183166a0616a8a15a83c36a38718c818c4a50
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-05-15 09:26:56 +0100

    Indent consistently in test/maxargs.ml

--- ocaml/compiler/test/maxargs.ml
+++ ocaml/compiler/test/maxargs.ml
@@ -4,18 +4,20 @@ open Compiler.Maxargs
 
  let tests =
    "max_args" >:::
-  ["returns the number of arguments to a print statement" >::
-    (fun _ ->
-      assert_equal
-        2
-        (max_args (Print [Number (1); Number (2)])));
+  [
+    "returns the number of arguments to a print statement" >::
+      (fun _ ->
+        assert_equal
+          2
+          (max_args (Print [Number (1); Number (2)]))
+      );
 
     "returns 0 for an assignment statement" >::
       (fun _ ->
         assert_equal
-        0
-        (max_args (Assignment ("x", Number (2))))
-        )
+          0
+          (max_args (Assignment ("x", Number (2))))
+      )
   ]
 
   let _ = run_test_tt_main tests
