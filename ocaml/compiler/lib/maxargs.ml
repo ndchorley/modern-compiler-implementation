2813c2c6ad2dcbcf67127f961a159d7b7dc4f1da
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-05-27 11:18:05 +0100

    Refactor to introduce args_for

--- ocaml/compiler/lib/maxargs.ml
+++ ocaml/compiler/lib/maxargs.ml
@@ -5,9 +5,9 @@ exception Todo
 let rec max_args (statement: statement) =
   match statement with
   | Print expressions -> List.length expressions
-  | Assignment (_, expression) -> (
-      match expression with
-      | StatementThenExpression (statement, _) -> max_args statement
-      | _ -> 0
-    )
+  | Assignment (_, expression) -> args_for expression
   | CompoundStatement (_, _) -> raise Todo
+  and args_for (expression: expression) =
+    match expression with
+    | StatementThenExpression (statement, _) -> max_args statement
+    | _ -> 0
