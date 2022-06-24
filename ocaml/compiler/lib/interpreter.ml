5e0e502b7a8f940798ae2da598bd17af81d0921e
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-06-24 11:56:34 +0100

    Evaluate the value of an identifier from a table containing the values

--- ocaml/compiler/lib/interpreter.ml
+++ ocaml/compiler/lib/interpreter.ml
@@ -1,16 +1,26 @@
 open Grammar
 open Stringutils
 
-let evaluate expression =
+let value_for name table =
+  (snd
+    (List.find
+      (fun name_and_value -> (fst name_and_value = name))
+      table
+    )
+  )
+
+let evaluate table expression =
   match expression with
   | Number (value) -> value
+  | Identifier (name) -> value_for name table
   | _ -> 0
 
 let interpret program write_line =
   match program with
   | Print expressions ->
       expressions
-      |> List.map (fun expression -> string_of_int (evaluate expression))
+      |> List.map
+          (fun expression -> string_of_int (evaluate [] expression))
       |> join " "
       |> write_line
   | _ -> ()
---
+++ ocaml/compiler/test/evaluate.ml
@@ -0,0 +1,16 @@
+open OUnit2
+open Compiler.Grammar
+open Compiler.Interpreter
+let tests =
+  "evaluating" >::: [
+    "an identifier produces its value" >::
+      (fun _ ->
+        let table = [("a", 1)] in
+
+        let result = evaluate table (Identifier ("a")) in
+
+          assert_equal 1 result ~printer:string_of_int
+      )
+  ]
+
+let _ = run_test_tt_main tests
